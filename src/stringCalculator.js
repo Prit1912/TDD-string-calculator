0ff7efd0d82895fd65d6b97e28dc2e2104e0feaf
Prit1912 <pritrojivadiya@gmail.com>
2024-10-02 09:12:27 +0530

    handle single number input for add function

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -3,6 +3,14 @@ function add(numberString) {
   if (!numberString?.length) {
     return 0;
   }
+
+  // check for single number input
+  if (numberString?.length === 1) {
+    if (!isNaN(parseInt(numberString))) {
+      return parseInt(numberString);
+    }
+    return 0;
+  }
 }
 
 module.exports = { add };
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -4,4 +4,11 @@ describe("Test cases for string calculator", () => {
   test("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  test("should return the number itself if it is valid number given as a string", () => {
+    expect(add("7")).toBe(7);
+    expect(add("9")).toBe(9);
+    expect(add("n")).toBe(0);
+    expect(add("p")).toBe(0);
+  });
 });
