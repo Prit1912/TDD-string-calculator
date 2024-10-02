7af7cd9b808cc96b7f7f74fc77a21a48c0ef2723
Prit1912 <pritrojivadiya@gmail.com>
2024-10-02 20:26:53 +0530

    Get add function called count

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,35 +1,51 @@
-function add(numberString) {
-  // check for empty string
-  if (!numberString?.length) {
-    return 0;
+class StringCalculator {
+  constructor() {
+    // Track the number of times add() is called
+    this.addFunctioncalledCount = 0;
   }
 
-  /* check for below input scenarios
-  1. Single number input
-  2. multiple number input separated by comma 
-  3. handle new line in input string
-  4. handle differnt custom delimeters
-  5. handle negative number
-  */
-  let delimiter = /[\n,]/; // Default delimiter
-  if (numberString.startsWith("//")) {
-    const parts = numberString.split("\n");
-    // Extract custom delimiter which is after //
-    delimiter = new RegExp(`[${parts[0][2]}]`);
-    numberString = parts[1];
+  add(numberString) {
+    // increase called count
+    this.addFunctioncalledCount++;
+
+    // check for empty string
+    if (!numberString?.length) {
+      return 0;
+    }
+
+    /* check for below input scenarios
+    1. Single number input
+    2. multiple number input separated by comma 
+    3. handle new line in input string
+    4. handle differnt custom delimeters
+    5. handle negative number
+    6. get how many times add function was called
+    */
+
+    let delimiter = /[\n,]/; // Default delimiter
+    if (numberString.startsWith("//")) {
+      const parts = numberString.split("\n");
+      // Extract custom delimiter which is after //
+      delimiter = new RegExp(`[${parts[0][2]}]`);
+      numberString = parts[1];
+    }
+
+    let numArray = numberString.split(delimiter);
+
+    // check for negative numbers
+    let negativeNumbers = numArray.filter((num) => parseInt(num) < 0);
+    if (negativeNumbers?.length) {
+      throw new Error(
+        `Negative numbers not allowed: ${negativeNumbers.join(",")}`
+      );
+    }
+
+    return numArray.reduce((sum, num) => sum + parseInt(num), 0);
   }
 
-  let numArray = numberString.split(delimiter);
-
-  // check for negative numbers
-  let negativeNumbers = numArray.filter((num) => parseInt(num) < 0);
-  if (negativeNumbers?.length) {
-    throw new Error(
-      `Negative numbers not allowed: ${negativeNumbers.join(",")}`
-    );
+  getAddFunctionCalledCount() {
+    return this.addFunctioncalledCount;
   }
-
-  return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
-module.exports = { add };
+module.exports = { StringCalculator };
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,37 +1,53 @@
-const { add } = require("../src/stringCalculator");
+const { StringCalculator } = require("../src/stringCalculator");
 
 describe("Test cases for string calculator", () => {
+  let strCalculator;
+
+  beforeEach(() => {
+    strCalculator = new StringCalculator();
+  });
+
   test("should return 0 for an empty string", () => {
-    expect(add("")).toBe(0);
+    expect(strCalculator.add("")).toBe(0);
   });
 
   test("should return the number itself if it is valid number given as a string", () => {
-    expect(add("7")).toBe(7);
-    expect(add("9")).toBe(9);
+    expect(strCalculator.add("7")).toBe(7);
+    expect(strCalculator.add("9")).toBe(9);
   });
 
   test("should return sum of multiple numbers given as a string", () => {
-    expect(add("1,2")).toBe(3);
-    expect(add("1,3,5")).toBe(9);
-    expect(add("2,4,6,8")).toBe(20);
-    expect(add("5,10,15,20,25")).toBe(75);
+    expect(strCalculator.add("1,2")).toBe(3);
+    expect(strCalculator.add("1,3,5")).toBe(9);
+    expect(strCalculator.add("2,4,6,8")).toBe(20);
+    expect(strCalculator.add("5,10,15,20,25")).toBe(75);
   });
 
-  it("should handle new lines between numbers", () => {
-    expect(add("1\n2,3")).toBe(6);
-    expect(add("1\n2\n3")).toBe(6);
-    expect(add("1,2\n3,4\n5,6,7")).toBe(28);
+  test("should handle new lines between numbers", () => {
+    expect(strCalculator.add("1\n2,3")).toBe(6);
+    expect(strCalculator.add("1\n2\n3")).toBe(6);
+    expect(strCalculator.add("1,2\n3,4\n5,6,7")).toBe(28);
   });
 
-  it("should handle custom delimiters starting with //", () => {
-    expect(add("//;\n1;2")).toBe(3);
-    expect(add("//|\n1|2|3")).toBe(6);
+  test("should handle custom delimiters starting with //", () => {
+    expect(strCalculator.add("//;\n1;2")).toBe(3);
+    expect(strCalculator.add("//|\n1|2|3")).toBe(6);
   });
 
-  it("should handle negative numbers", () => {
-    expect(() => add("//;\n1;-2")).toThrow("Negative numbers not allowed: -2");
-    expect(() => add("//|\n1|-2|-3")).toThrow(
+  test("should handle negative numbers", () => {
+    expect(() => strCalculator.add("//;\n1;-2")).toThrow(
+      "Negative numbers not allowed: -2"
+    );
+    expect(() => strCalculator.add("//|\n1|-2|-3")).toThrow(
       "Negative numbers not allowed: -2,-3"
     );
+  });
+
+  test("should return the number of times add() has been called", () => {
+    expect(strCalculator.getAddFunctionCalledCount()).toBe(0); // No call yet, should be 0
+    strCalculator.add("1,2");
+    expect(strCalculator.getAddFunctionCalledCount()).toBe(1); // One call, should be 1
+    strCalculator.add("3,4");
+    expect(strCalculator.getAddFunctionCalledCount()).toBe(2); // Two calls, should be 2
   });
 });
